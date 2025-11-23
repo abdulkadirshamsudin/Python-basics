f9fd3ba0c7689793cea546a020827582f353b3b5
Abdulkadir Shamsudin <abdulkadir.shamsudin@strathmore.edu>
2025-11-23 12:48:02 +0300

    firs code pushed to github

---
+++ foundations.py
@@ -0,0 +1,11 @@
+# foundations.py
+# This is our first Python file for testing GitHub workflow
+
+# Print a welcome message
+print("Hello, Python world!")
+
+# Simple calculation
+a = 5
+b = 3
+sum_ab = a + b
+print("The sum of", a, "and", b, "is", sum_ab)
